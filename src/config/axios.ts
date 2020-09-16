31b64f6fcd37d15a21bd02859a88baccc77269f0
Mitchell Hollander <MitchellSHollander@gmail.com>
2020-09-16 15:51:07 -0500

    added images

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,10 +1,12 @@
 import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
+  baseURL: 'https://itunes.apple.com',
+  withCredentials: false,
   headers: {
-	  'Access-Control-Allow-Origin': '*',
-	},
-  baseURL: 'https://itunes.apple.com'
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+  },
 });
 
 export default instance;
